d7bbd9358fe472a6e615070a877a48a8ead3b9de
jfcardoso <maestro.jfc@gmail.com>
2022-04-25 21:28:43 -0300

    Ajustando Migration para valores terem default zero.

--- teste-gigalink/src/main/resources/db/migration/V6__create_table_pedido.sql
+++ teste-gigalink/src/main/resources/db/migration/V6__create_table_pedido.sql
@@ -2,9 +2,9 @@ CREATE TABLE public.pedido (
 	id SERIAL NOT NULL,
 	data_hora timestamp NOT NULL,
 	nota_fiscal varchar(80) not null,
-	valor_frete decimal(10, 2) not null,
-	desconto decimal(10, 2) null,
-	valor_total decimal(10, 2) not null,
+	valor_frete decimal(10, 2) not null default 0,
+	desconto decimal(10, 2) not null default 0,
+	valor_total decimal(10, 2) not null default 0,
 	transportadora_id integer null,
 	primary key (id),
 	CONSTRAINT fk_transportadora FOREIGN KEY(transportadora_id) REFERENCES transportadora(id)
