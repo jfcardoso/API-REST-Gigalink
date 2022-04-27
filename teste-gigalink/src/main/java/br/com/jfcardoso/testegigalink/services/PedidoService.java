6fc44343c18b0a38315164351f89b1cc521e13ac
jfcardoso <maestro.jfc@gmail.com>
2022-04-27 19:45:54 -0300

    Adicionando o método validarValorTotal() do pedido passando o item, valor do frete e desconto.

--- teste-gigalink/src/main/java/br/com/jfcardoso/testegigalink/services/PedidoService.java
+++ teste-gigalink/src/main/java/br/com/jfcardoso/testegigalink/services/PedidoService.java
@@ -1,11 +1,13 @@
 package br.com.jfcardoso.testegigalink.services;
 
-import br.com.jfcardoso.testegigalink.entities.Fornecedor;
+import br.com.jfcardoso.testegigalink.entities.Item;
 import br.com.jfcardoso.testegigalink.entities.Pedido;
 import br.com.jfcardoso.testegigalink.repositories.PedidoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
@@ -13,8 +15,8 @@ import java.util.Optional;
 @Service
 public class PedidoService {
 
-   @Autowired
-   PedidoRepository pedidoRepository;
+    @Autowired
+    PedidoRepository pedidoRepository;
 
     public List<Pedido> getAll() {
         return pedidoRepository.findAll();
@@ -22,18 +24,21 @@ public class PedidoService {
 
     public Pedido getById(Long id) {
         Optional<Pedido> pedidoOpt = pedidoRepository.findById(id);
-        if (pedidoOpt.isEmpty()){
+        if (pedidoOpt.isEmpty()) {
             throw new RuntimeException("Pedido não cadastrado");
         }
         return pedidoOpt.get();
     }
 
     public Pedido save(Pedido pedido) {
-        if (pedido.getItens()==null || pedido.getItens().isEmpty()){
+        if (pedido.getItens() == null || pedido.getItens().isEmpty()) {
             throw new RuntimeException("É necessário pelo menos um item no Pedido.");
         }
+
+        validarValorTotal(pedido);
+
         pedido.getItens().forEach(i -> i.setPedido(pedido));
-        if (pedido.getDataHora()== null){
+        if (pedido.getDataHora() == null) {
             pedido.setDataHora(new Date());
         }
         return pedidoRepository.save(pedido);
@@ -43,4 +48,17 @@ public class PedidoService {
         pedidoRepository.deleteById(id);
     }
 
-  }
+    private void validarValorTotal(Pedido pedido) {
+        BigDecimal valorTotalDosItens = new BigDecimal(0);
+        for (Item item : pedido.getItens()) {
+            valorTotalDosItens = valorTotalDosItens.add(item.getValor().multiply(new BigDecimal(item.getQuantidade()).setScale(0, RoundingMode.HALF_EVEN)))
+                    .setScale(2, RoundingMode.HALF_EVEN);
+        }
+        BigDecimal valorTotalValidado = valorTotalDosItens.add(pedido.getDesconto().negate())
+                .add(pedido.getValorFrete()).setScale(2, RoundingMode.HALF_EVEN);
+
+        if (!valorTotalValidado.equals(pedido.getValorTotal())) {
+            throw new RuntimeException("O valor total do pedido está inconsistente.");
+        }
+    }
+}
